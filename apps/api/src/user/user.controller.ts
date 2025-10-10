747f4f62a84f431a915fc715ecc0084c5ed9d697
abhinav.arya <abhinav.arya@scaleupally.io>
2025-10-10 16:46:43 +0530

    feat: adding CRUD functionality

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -1,28 +1,44 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
-
+import { UserService } from 'src/user/user.service';
 @Controller('user')
 export class UserController {
+    constructor(private readonly userServices: UserService) { }
     @Get()
-    getAll(): string {
-        return 'this is cat'
+    async getAll(): Promise<any> {
+        const records = await this.userServices.getAllUsers()
+        return {
+            status: true,
+            message: "User fetched successfully",
+            data: records
+        }
     }
 
     @Get(':id')
-    get(@Param('id') id: number): string {
-        return 'this is user with id' + id;
+    async get(@Param('id') id: number): Promise<any> {
+        const resp = await this.userServices.getUser(Number(id))
+        return {
+            status: true,
+            message: "User fetched successfully",
+            data: resp
+        }
     }
 
     @Post('')
-    create(@Body() data: any): string {
-        console.log(data)
-        return "Creted Successfully"
+    create(@Body() data: any): any {
+        this.userServices.createUser(data);
+        return {
+            status: true,
+            message: "User created successfully",
+        }
     }
 
     @Put(':id')
-    update(@Body() data: any, @Param('id') id: number): string {
-        console.log('id', id)
-        console.log('data', data)
-        return 'Updated Successfully'
+    update(@Body() data: any, @Param('id') id: number): any {
+        this.userServices.updateUser(Number(id), data)
+        return {
+            status: true,
+            message: "User updated successfully",
+        }
     }
 
     @Patch(':id')
@@ -31,7 +47,11 @@ export class UserController {
     }
 
     @Delete(':id')
-    hardDelete(@Param('id') id: number): string {
-        return 'hard delete successfull' + id;
+    hardDelete(@Param('id') id: number): any {
+        this.userServices.deleteUser(Number(id))
+        return {
+            status: true,
+            message: "User Deleted successfully",
+        }
     }
 }
--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -1,6 +1,34 @@
 import { Injectable } from '@nestjs/common';
-
+import { PrismaService } from 'prisma/prisma.service';
 @Injectable()
 export class UserService {
-    
+    constructor(private prisma: PrismaService) { }
+    async getAllUsers() {
+        return await this.prisma.user.findMany();
+    }
+    async createUser(data: any) {
+        return await this.prisma.user.create({data});
+    }
+    async getUser(id: number) {
+        return await this.prisma.user.findFirst({
+            where: {
+                id: id
+            }
+        });
+    }
+    async updateUser(id: number, data: any) {
+        return await this.prisma.user.update({
+            where: {
+                id: id
+            },
+            data: data
+        });
+    }
+    async deleteUser(id: number) {
+        return await this.prisma.user.delete({
+            where: {
+                id: id
+            }
+        })
+    }
 }
