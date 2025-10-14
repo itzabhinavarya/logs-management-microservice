8814ed58746a68cb0afceaa9086d6f0de2e50e66
abhinav.arya <abhinav.arya@scaleupally.io>
2025-10-14 18:58:06 +0530

    fix: adding config for cloudwatch

---
+++ apps/logger/src/utils/readLogs.ts
@@ -0,0 +1,18 @@
+// import { CloudWatchLogsClient, GetLogEventsCommand } from '@aws-sdk/client-cloudwatch-logs';
+
+
+export const readLogs = async () => {
+    // const client = new CloudWatchLogsClient({
+    //     region: 'ap-south-1',
+    //     endpoint: 'http://localhost:4566', // LocalStack endpoint
+    //     credentials: { accessKeyId: 'test', secretAccessKey: 'test' },
+    // });
+
+    // const response = await client.send(new GetLogEventsCommand({
+    //     logGroupName: '/microservices/logger',
+    //     logStreamName: 'logger-dev',
+    // }));
+
+    // console.log(response.events);
+
+}
