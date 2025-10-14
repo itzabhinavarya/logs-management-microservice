f865ac7843a7a7d313a02dbe6f8387d5780ab12d
abhinav.arya <abhinav.arya@scaleupally.io>
2025-10-14 13:58:38 +0530

    fix: adding single .env for all services

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+  console.log("process.env.PORTs", process.env.API_PORT)
+  await app.listen(process.env.API_PORT ?? 4000);
 }
 bootstrap();
