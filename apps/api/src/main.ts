b3e168bec66ca43eb4b9b39f16e5359fb92b4eff
abhinav.arya <abhinav.arya@scaleupally.io>
2025-10-16 15:28:37 +0530

    feat: adding config

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    disableErrorMessages: false,
+    whitelist: true,
+    transform: true
+  }));
   await app.listen(process.env.API_PORT ?? 4000);
 }
 bootstrap();
