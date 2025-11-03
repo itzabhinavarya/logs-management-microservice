14d3c298e23105640e870ccabf90cc9ffaf581a5
abhinav.arya <abhinav.arya@scaleupally.io>
2025-11-03 11:55:14 +0530

    fix: adding comment in tast

--- apps/api/src/task/task.module.ts
+++ apps/api/src/task/task.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 import { LoggerModule } from 'src/infrastructure/logger.module';
-
+// 
 @Module({
   imports: [LoggerModule],
   controllers: [TaskController],
