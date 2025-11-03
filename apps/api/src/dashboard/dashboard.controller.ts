35633385eb672da2c553c605c4fa2b8a1b090361
abhinav.arya <abhinav.arya@scaleupally.io>
2025-11-03 13:41:27 +0530

    feat: adding logs for dashboard service

--- apps/api/src/dashboard/dashboard.controller.ts
+++ apps/api/src/dashboard/dashboard.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { DashboardService } from 'src/dashboard/dashboard.service';
+import { LoggerClient } from 'src/infrastructure/logger.client';
 
 @Controller('dashboard')
 export class DashboardController {
-    constructor(private readonly service: DashboardService) { }
+    
+    constructor(
+        private readonly service: DashboardService,
+        private readonly loggerClient: LoggerClient
+    ) { }
 
     @Get('/')
     getAllStats() {
+        this.loggerClient.log('Fetched all stats for dashboard', 'info');
         return this.service.getStats()
     }
 }
--- apps/api/src/dashboard/dashboard.module.ts
+++ apps/api/src/dashboard/dashboard.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
+import { LoggerClient } from 'src/infrastructure/logger.client';
 
 @Module({
   controllers: [DashboardController],
-  providers: [DashboardService]
+  providers: [DashboardService, LoggerClient]
 })
-export class DashboardModule {}
+export class DashboardModule { }
