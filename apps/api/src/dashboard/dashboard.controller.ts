9b00742012796d415568ee4f280d38c61e4edf9b
abhinav.arya <abhinav.arya@scaleupally.io>
2025-11-03 14:04:17 +0530

    fix: filter to fetch task by project id

--- apps/api/src/dashboard/dashboard.controller.ts
+++ apps/api/src/dashboard/dashboard.controller.ts
@@ -1,18 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { DashboardService } from 'src/dashboard/dashboard.service';
 import { LoggerClient } from 'src/infrastructure/logger.client';
+import { response } from 'src/utils/response';
 
 @Controller('dashboard')
 export class DashboardController {
-    
+
     constructor(
         private readonly service: DashboardService,
         private readonly loggerClient: LoggerClient
     ) { }
 
-    @Get('/')
-    getAllStats() {
+    @Get('/stats')
+    async getAllStats() {
+        this.loggerClient.log('Fetching all stats for dashboard', 'info');
+        const data = await this.service.getStats()
         this.loggerClient.log('Fetched all stats for dashboard', 'info');
-        return this.service.getStats()
+        return response({
+            status: true,
+            statusCode: 200,
+            message: 'All stats fetched successfully',
+            data
+        })
     }
 }
